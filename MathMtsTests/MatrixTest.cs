f4999ee9b02c2590a9f61d5a740367aa4b04b8d8
JackLanger <jacek.l271186@gmail.com>
2021-12-24 14:49:25 +0100

    auto: some project changes have been done by the system

--- MathMtsTests/MatrixTest.cs
+++ MathMtsTests/MatrixTest.cs
@@ -1,31 +1,30 @@
 using MathMTS.algebra;
 using NUnit.Framework;
 
-namespace MathMtsTests
+namespace MathMtsTests;
+
+public class MatrixTest
 {
-  public class MatrixTest
-  {
     [SetUp]
     public void Setup()
     {
     }
 
     [Test]
-    public void Test1()
+    public void MultiplyTest()
     {
-      Matrix first = new Matrix(new double[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } });
-      Matrix second = new Matrix(new double[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } });
-      Matrix expected = new Matrix(new double[,] { { 30, 66, 102 }, { 36, 81, 126 }, { 42, 96, 150 } });
-      Matrix actual = first * second;
+        Matrix first = new Matrix(new double[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } });
+        Matrix second = new Matrix(new double[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } });
+        Matrix expected = new Matrix(new double[,] { { 30, 66, 102 }, { 36, 81, 126 }, { 42, 96, 150 } });
+        Matrix actual = first * second;
 
-      int index = 0;
-      for (int h = 0; h < actual.MatrixArray.Length / actual.Width; h++)
-      {
-        for (int w = 0; w < actual.Width; w++)
+        int index = 0;
+        for (int h = 0; h < actual.MatrixArray.Length / actual.Width; h++)
         {
-          Assert.AreEqual(expected.MatrixArray[h, w], actual.MatrixArray[h, w]);
+            for (int w = 0; w < actual.Width; w++)
+            {
+                Assert.AreEqual(expected.MatrixArray[h, w], actual.MatrixArray[h, w]);
+            }
         }
-      }
     }
-  }
 }
